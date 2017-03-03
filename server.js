4d7557088fabf36503f76cce3bcba722c72fcdb8
arnuga3 <arniszelcs@gmail.com>
2017-03-03 16:21:01 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var request = require('request');
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// lat, lng (from a client) to request force, neighbourhood form API
+app.post('/neighbourhood', function(req,res) {
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.writeHead(200, {"Content-Type": "application/json"});
+
+  var lat = req.body.lat;
+  var lng = req.body.lng;
+  console.log("Request Coordinates: " + lat + " : " + lng);
+
+  // GET request using 'request'
+  request.get({
+    headers: {'Content-Type': 'application/json'},
+    url: 'https://data.police.uk/api/locate-neighbourhood?q=' + lat + ',' + lng
+  }, function(error, response, body){
+    console.log(body);
+    // Return to a client
+    res.end(body);
+  });
+});
+
+
+// Neighbourhood full details
+app.post('/neighbourhood/details', function(req,res) {
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.writeHead(200, {"Content-Type": "application/json"});
+
+  var force = req.body.force;
+  var neighbourhood = req.body.neighbourhood;
+  console.log("Requested: " + force + " | " + neighbourhood);
+
+  // GET request using 'request'
+  request.get({
+    headers: {'Content-Type': 'application/json'},
+    url: 'https://data.police.uk/api/' + force + '/' + neighbourhood
+  }, function(error, response, body){
+    console.log(body);
+    // Return to a client
+    res.end(body);
+  });
+});
+
+
+
+app.listen(8080,function(){
+  console.log("Started on PORT 8080");
+});
