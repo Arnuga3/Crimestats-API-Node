37061f06557e2bb40bd029c29353d77626c9d1a8
arnuga3 <arniszelcs@gmail.com>
2017-03-05 00:14:36 +0000

    fasda

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.post('/neighbourhood/details', function(req,res) {
 
 
 // A list of crime categories
-app.post('/crime-categories', function(req,res) {
+app.post('/crime-cat-data', function(req,res) {
 
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -105,7 +105,7 @@ app.post('/crime-categories', function(req,res) {
       + numOfEachCat[i].num);
   }
 
-  
+
 });
 
 /*
