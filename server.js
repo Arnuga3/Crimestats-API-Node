5861e03547bcb7f7a0f75a5f85fd305b5f5b3f69
arnuga3 <arniszelcs@gmail.com>
2017-03-05 00:10:59 +0000

    first attempt to put logic on server - test

--- server.js
+++ server.js
@@ -60,24 +60,58 @@ app.post('/crime-categories', function(req,res) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.writeHead(200, {"Content-Type": "application/json"});
 
+  var poly = req.body;
+  console.log("POLY: " + poly);
   console.log("Requested: Crime categories");
 
   // GET request using 'request'
-  request.get({
-    headers: {'Content-Type': 'application/json'},
-      url: 'https://data.police.uk/api/crime-categories'
+  var requests = ['https://data.police.uk/api/crime-categories',
+                  'https://data.police.uk/api/crimes-street/all-crime?poly=' + poly];
+  var responses = [];
+  for (i in requests) {
+    request.get({
+      headers: {'Content-Type': 'application/json'},
+      url: requests[i]
     }, function(error, response, body){
-      var cats = [];
-      var data = JSON.parse(body);
-      for(var i=0; i<data.length; i++) {
-        cats.push(data[i].name);
-        console.log(data[i].name);
-      }
-    // Return to a client
-    res.end(JSON.stringify(cats));
+      responses.push(body);
+    });
+  }
+  var categories = [];
+  var data = JSON.parse(responses[0]);
+  for(var i=0; i<data.length; i++) {
+    categories.push(data[i].name);
+    console.log(data[i].name);
+  }
+  // Array to store objects with category and number of crimes
+  var numOfEachCat = [];
+  // Assigning 0 default values to each category
+  for(var i=0;i<categories.length; i++) {
+    numOfEachCat.push({cat: [categories[i]], num: 0});
+  }
+  var crimeData = JSON.parse(responses[1]);
+  // Counting crimes
+  for(var i=1;i<crimeData.length; i++) {
+    for (var j=0; j<numOfEachCat.length; j++) {
+      if (data[i].category == numOfEachCat[j].cat)
+      numOfEachCat[j].num += 1;
+    }
+  }
+  // Sorting by number, high to low
+  numOfEachCat.sort(function(a, b){
+      return b.num-a.num;
   });
+  for(var i=0;i<numOfEachCat.length; i++) {
+    console.log("category: " + numOfEachCat[i].cat + " | num: "
+      + numOfEachCat[i].num);
+  }
+
+  
 });
 
+/*
+
+
+*/
 
 app.get('/', function(req,res) {
   res.send("Hello to CrimeStats! <br> Documentation is coming...");
