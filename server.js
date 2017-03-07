fc3499583d117a682e0aa409758deaa7a34ad304
arnuga3 <arniszelcs@gmail.com>
2017-03-07 23:54:40 +0000

    output test

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.post('/force', function(req, res) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.writeHead(200, {"Content-Type": "application/json"});
   console.log(req.body);
-  var lat = req.body.centerPoint.lat;
-  var lng = req.body.centerPoint.lng;
+  var lat = req.body.centerPoint[lat];
+  var lng = req.body.centerPoint[lng];
   var onMapIDs = req.body.onMapIDs;
   console.log(lat);
   console.log(lng);
