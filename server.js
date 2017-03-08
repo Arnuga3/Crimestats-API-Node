7ab7a27ea19cccbe8348b2c8cd996aee7a5f9b4a
arnuga3 <arniszelcs@gmail.com>
2017-03-08 02:00:20 +0000

    output test

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post('/force', function(req, res) {
     url: 'https://data.police.uk/api/locate-neighbourhood?q=' + lat + ',' + lng
   }, function(error, response, body){
     console.log(body);
-    force = body.force;
+    force = body.query.force;
       console.log(force);
     request.get({
       headers: {'Content-Type': 'application/json'},
