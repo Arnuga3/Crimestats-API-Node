f45f42dc12ef4d7555aa9fc3e72d779a349aca88
arnuga3 <arniszelcs@gmail.com>
2017-03-08 00:17:24 +0000

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var request = require('request');
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 
 app.post('/force', function(req, res) {
