577c38cb9189240080078d790d7224401b6ebe6b
arnuga3 <arniszelcs@gmail.com>
2017-03-09 12:05:35 +0000

    some comments

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.post('/force', function(req, res) {
       var urls = [];
       var onMapViewNeighb = [];
       var contains = function(a, b) {
-        console.log(b.lat < a.botR.lat);
+        console.log(b.lng > a.topL.lng);
         return b.lat > a.topL.lat &&
                 b.lat < a.botR.lat &&
                 b.lng > a.topL.lng &&
