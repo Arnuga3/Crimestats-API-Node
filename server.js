fddcfbd6151e871af134e53ae6a6187e5cfb23ba
arnuga3 <arniszelcs@gmail.com>
2017-03-10 02:11:57 +0000

    contains() test

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post('/force', function(req, res) {
       // async module to handle multiple requests and combine all the results
       asynch.each(urls, function(url, callback) {
           request(url, function(err, response, body) {
-            console.log(url);
+            console.log(body);
             obj = JSON.parse(body);
             responses.push({id: obj.id, lat: obj.centre.latitude, lng: obj.centre.longitude});
 
