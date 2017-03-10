a03c117c78990b947062c09f928ffd8362de751c
arnuga3 <arniszelcs@gmail.com>
2017-03-10 12:17:30 +0000

    test

--- server.js
+++ server.js
@@ -70,8 +70,8 @@ app.post('/force', function(req, res) {
       // async module to handle multiple requests and combine all the results
       asynch.each(urls, function(url, callback) {
           request(url, function(err, response, body) {
-            console.log(body);
-            obj = JSON.parse(body, null, 3);
+            //console.log(body);
+            obj = JSON.parse(body);
             responses.push({id: obj.id, lat: obj.centre.latitude, lng: obj.centre.longitude});
 
             callback();
@@ -83,7 +83,7 @@ app.post('/force', function(req, res) {
           }
           console.log(responses.length);
 
-            var inside = [];
+            //var inside = [];
             /**/
             /*asynch.each(responses, function(el, callback) {
                 console.log(contains(rectangle, el));
