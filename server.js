57f9c3ee93ba554e6455d6e7c4114f2b6f702abc
arnuga3 <arniszelcs@gmail.com>
2017-03-12 08:01:34 +0000

    poly sliced on 2 and 2 requests sent

--- server.js
+++ server.js
@@ -221,7 +221,7 @@ app.post('/crime-cat-data', function(req, res) {
             if(response.statusCode == 200) {
                 var crimeData = JSON.parse(body);
                 splitResponses.push(crimeData);
-                cosole.log(splitResponses.length);
+                console.log(splitResponses.length);
                 callback();
             } else {
               console.log(response.statusCode);
