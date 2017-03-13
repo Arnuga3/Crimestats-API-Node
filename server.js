31549ebb66700f0a2718797c80f86f62f5c55500
arnuga3 <arniszelcs@gmail.com>
2017-03-13 23:49:42 +0000

    try to slice on 4

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ function convertToPoly(arr) {
 }
 
 // Accepts 4 points of a map view and slices the view on half
-function splitOn2(bigPoly) {
+function splitOn2(bigPoly, categories) {
   var poly1 = [];
   var poly2 = [];
 
@@ -269,7 +269,7 @@ app.post('/crime-cat-data', function(req, res) {
           console.log("FAIL - 503");
           console.log("RECOVERY...");
 
-          var crimes = splitOn2(poly);
+          var crimes = splitOn2(poly, categories);
           if (crimes) res.end(JSON.stringify(crimes));
         }
     });
